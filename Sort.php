2c767a7d3c9faf4b27f4bff7eade2faf0c835f04
liujidong <liujidong@guazi.com>
2017-09-07 11:43:49 +0800

    one

---
+++ Sort.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jidong
+ * Date: 2017/9/7
+ * Time: 上午11:42
+ */
+class Sort
+{
+    public static function run(){
+        echo 'test';
+    }
+}
+Sort::run();
